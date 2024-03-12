234d9664cd349a3582079e35e6b360422d2d717c
MATHEUS NORONHA DA SILVA <23111820@dsn127m1242375.cfp127aluno.local>
2024-03-12 11:57:52 -0300

    finally

--- app/src/main/java/br/senai/sp/jandira/calculoimc/MainActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/calculoimc/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -15,10 +16,13 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
@@ -27,8 +31,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -57,7 +63,7 @@ fun Greeting() {
         Box (
             contentAlignment = Alignment.TopCenter,
             modifier = Modifier
-                .background(Color.Red)
+                .background(Color(0xffea1450))
                 .fillMaxWidth()
                 .height(200.dp)
         ){
@@ -75,31 +81,41 @@ fun Greeting() {
                 Text(
                     text = "Calculadora IMC",
                     fontSize = 25.sp,
-                    color = Color.White
+                    color = Color.White,
+                    fontWeight = FontWeight.Bold
                 )
             }
         }
         Column (
+            horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier
                 .fillMaxWidth()
-                .fillMaxHeight()
-                .background(Color.Yellow),
-            horizontalAlignment = Alignment.CenterHorizontally
+                .fillMaxHeight(),
         ){
             Box (
                 modifier = Modifier
-                    .background(Color.Green)
-                    .height(400.dp),
+                    .offset(x = 0.dp, y = -30.dp)
+                    .shadow(
+                        elevation = 8.dp,
+                        shape = RoundedCornerShape(10.dp),
+                    )
+                    .background(Color(0xFFf7f3f2))
+                    .height(380.dp)
+                    .width(300.dp),
                 contentAlignment = Alignment.TopCenter
             ){
-                Column {
+                Column (
+                    modifier = Modifier
+                        .padding(vertical = 20.dp)
+                ){
                     Box (
                         modifier = Modifier.align(alignment = Alignment.CenterHorizontally)
                     ){
                         Text(
                             text = "Seus dados",
-                            color = Color.Red,
-                            fontSize = 30.sp
+                            color = Color(0xffea1450),
+                            fontSize = 30.sp,
+                            fontWeight = FontWeight.Bold
                         )
 
                     }
@@ -107,47 +123,109 @@ fun Greeting() {
                         horizontalAlignment = Alignment.Start,
                         modifier = Modifier
                             .padding(20.dp)
-                            .background(Color.Blue)
+                            .fillMaxHeight()
                     ){
-                        Text(text = "Seu peso")
+                        Text(
+                            text = "Seu peso",
+                            color = Color(0xffea1450)
+                        )
+                        Spacer(modifier = Modifier.height(10.dp))
                         OutlinedTextField(
                             value = "",
                             onValueChange = {},
                             placeholder = {
-                                Text(text = "Insira seu peso")
+                                Text(
+                                    text = "Seu peso em Kg",
+                                    color = Color(0xFFA5A5A5)
+                                )
+                            },
+                            colors = OutlinedTextFieldDefaults.colors(
+                                unfocusedBorderColor = Color(0xffea1450)
+                            ),
+                            modifier = Modifier
+                                .background(Color.White, shape = RoundedCornerShape(10.dp))
+                                .fillMaxWidth()
+                        )
+                        Spacer(modifier = Modifier.height(20.dp))
+                        Text(
+                            text = "Sua altura",
+                            color = Color(0xffea1450)
+                        )
+                        Spacer(modifier = Modifier.height(10.dp))
+                        OutlinedTextField(
+                            value = "",
+                            onValueChange = {},
+                            placeholder = {
+                                Text(
+                                    text = "Sua altura em cm",
+                                    color = Color(0xFFA5A5A5),
+                                )
                             },
                             colors = OutlinedTextFieldDefaults.colors(
                                 unfocusedBorderColor = Color.Red
                             ),
                             modifier = Modifier
-                                .background(Color.White)
-                                .width(200.dp)
+                                .background(Color.White, shape = RoundedCornerShape(10.dp))
+                                .fillMaxWidth()
                         )
-                        Text(text = "Sua altura")
-                        OutlinedTextField(
-                            value = "",
-                            onValueChange = {},
-                            placeholder = {
-                                Text(text = "Insira sua altura")
-                            },
-                            colors = OutlinedTextFieldDefaults.colors(
-                                unfocusedBorderColor = Color.Red
-                            ),
-                            modifier = Modifier.background(Color.White)
-                        )
-                    }
-                    Button(onClick = { /*TODO*/ }) {
-
+                        Button(
+                            onClick = { /*TODO*/ },
+                            shape = RoundedCornerShape(10.dp),
+                            modifier = Modifier
+                                .height(50.dp)
+                                .offset(y = 35.dp)
+                                .fillMaxWidth(),
+                            colors = ButtonDefaults.buttonColors(containerColor = Color(0xffea1450)),
+                        ) {
+                            Text(
+                                text = "CALCULAR",
+                                fontSize = 20.sp
+                            )
+                        }
                     }
                 }
             }
-        }
-        Box {
-            Row {
-                Column {
 
+            Box (
+                modifier = Modifier
+                    .shadow(
+                        elevation = 10.dp,
+                        shape = RoundedCornerShape(10.dp)
+                    )
+                    .border(width = 2.dp, Color(0xffea1450), shape = RoundedCornerShape(10.dp))
+                    .background(Color(0xff2d9560), shape = RoundedCornerShape(10.dp))
+                    .width(300.dp)
+                    .height(120.dp)
+
+            ){
+                Row (
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .padding(horizontal = 20.dp)
+                ){
+                    Column (
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ){
+                        Text(
+                            text = "Resultado",
+                            fontSize = 15.sp,
+                            color = Color.White
+                        )
+                        Text(
+                            text = "Peso Ideal",
+                            fontSize = 25.sp,
+                            color = Color.White
+                        )
+                    }
+                    Spacer(modifier = Modifier.width(30.dp))
+                    Text(
+                        text = "21.3",
+                        fontSize = 50.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.White
+                    )
                 }
-                Text(text = "")
             }
         }
     }
